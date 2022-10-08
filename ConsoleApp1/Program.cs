80217cfa3c41564ba17aeb4c0db9bd0345a420f6
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-08 15:20:35 +0900

    LU分解の計算結果の表示方法を変更

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,17 +1,4 @@
-﻿void GaussJordan(float[,] A)    // ガウスジョルダン法で逆行列に変換
-{
-    var n = A.GetLength(0);
-    for (var k = 0; k < n; ++k)
-    {
-        var w = 1 / A[k, k];
-        A[k, k] = w;
-        for (var j = 0; j < n; ++j) if (j != k) A[k, j] *= w;
-        for (var i = 0; i < n; ++i) if (i != k) for (var j = 0; j < n; ++j) if (j != k) A[i, j] -= A[i, k] * A[k, j];
-        for (var i = 0; i < n; ++i) if (i != k) A[i, k] *= -w;
-    }
-}
-
-void LUDecomposition(float[,] A)    // LU分解：LとUを重ねた行列に変換
+﻿void LUDecomposition(float[,] A)    // LU分解：LとUを重ねた行列に変換
 {
     var n = A.GetLength(0);
     for (var k = 0; k < n; ++k)
@@ -35,5 +22,9 @@ float[,] A = new float[,]    // 正方行列
 
 
 LUDecomposition(A);
+Console.Write("L = " + Environment.NewLine);
 for (int i = 0; i < A.GetLength(0); ++i) for (int j = 0; j < A.GetLength(1); ++j)
-        Console.Write(A[i, j].ToString("F3") + (j < A.GetLength(1) - 1 ? "\t" : Environment.NewLine));
+        Console.Write((i > j ? A[i, j] : i == j ? 1 : 0).ToString("F3") + (j < A.GetLength(1) - 1 ? "\t" : Environment.NewLine));
+Console.Write(Environment.NewLine + "U = " + Environment.NewLine);
+for (int i = 0; i < A.GetLength(0); ++i) for (int j = 0; j < A.GetLength(1); ++j)
+        Console.Write((i <= j ? A[i, j] : 0).ToString("F3") + (j < A.GetLength(1) - 1 ? "\t" : Environment.NewLine));
