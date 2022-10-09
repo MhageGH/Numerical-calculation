56cb8e9bcda684afaa75793d6218e116c66783b6
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-09 21:00:22 +0900

    ルンゲクッタ法を追加

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,11 +12,24 @@ double ModifiedEuler(double x, double h, double t)  // 修正オイラー法
     return x + h * (f(x, t) + f(x + h * f(x, t), t + h)) / 2.0;
 }
 
+double RungeKutta(double x, double h, double t)     // ルンゲクッタ法
+{
+    double[] x_ = new double[3], f_ = new double[4];
+    f_[0] = f(x, t);
+    x_[0] = x + h * f_[0] / 2;
+    f_[1] = f(x_[0], t + h / 2);
+    x_[1] = x + h * f_[1] / 2;
+    f_[2] = f(x_[1], t + h / 2);
+    x_[2] = x + h * f_[2];
+    f_[3] = f(x_[2], t + h);
+    return x + h * (f_[0] + 2 * f_[1] + 2 * f_[2] + f_[3]) / 6;
+}
+
 const double t0 = 0, t1 = 1.6;  // 定義域
 const double x0 = 0;            // 初期値
 const double h = 0.05;          // 刻み幅
 const double e = 0.001;         // 定義域拡大量(浮動小数点の誤差による終端部抜け防止)
-var methodName = new String[] { "オイラー法：", "修正オイラー法：" };
+var methodName = new String[] { "オイラー法：", "修正オイラー法：", "ルンゲクッタ法："};
 
 for (int i = 0; i < methodName.Length; i++)
 {
@@ -27,6 +40,7 @@ for (int i = 0; i < methodName.Length; i++)
         error = Math.Max(Math.Abs(x - Math.Tanh(t)), error);    // 厳密解tanh(t)との差
         if (i == 0) x = Euler(x, h, t);
         else if (i == 1) x = ModifiedEuler(x, h, t);
+        else if (i == 2) x = RungeKutta(x, h, t);
     }
     Console.WriteLine("誤差の最大値 = " + error.ToString("F10") + Environment.NewLine);
 }
