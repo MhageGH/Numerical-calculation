09e18b14932508fc5d9feea503af63f9e48333b4
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-10 21:27:56 +0900

    惑星の軌道計算

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,46 +1,49 @@
-﻿double f (double x, double t) { // 微分方程式の右辺 ⇒ 厳密解 tanh(t)
-    return 1.0 - x * x; 
-}
-
-double Euler(double x, double h, double t)  // オイラー法
+﻿var fs = new Func<double[], double, double>[]   // 微分方程式の右辺(ベクトルに対応)
 {
-    return x + h * f(x, t);
-}
-
-double ModifiedEuler(double x, double h, double t)  // 修正オイラー法
+    (xs, t) => xs[2],   // xs はベクトル(x, y, u, v)
+    (xs, t) => xs[3],
+    (xs, t) => -xs[0] / Math.Pow(xs[0] * xs[0] + xs[1] * xs[1], 3.0 / 2),
+    (xs, t) => -xs[1] / Math.Pow(xs[0] * xs[0] + xs[1] * xs[1], 3.0 / 2)
+};
+double[] Euler(double[] xs, double h, double t)  // オイラー法
 {
-    return x + h * (f(x, t) + f(x + h * f(x, t), t + h)) / 2.0;
+    var xs_next = new double[xs.Length];
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * fs[i](xs, t);
+    return xs_next;
 }
-
-double RungeKutta(double x, double h, double t)     // ルンゲクッタ法
+double[] ModifiedEuler(double[] xs, double h, double t)  // 修正オイラー法
 {
-    double[] x_ = new double[3], f_ = new double[4];
-    f_[0] = f(x, t);
-    x_[0] = x + h * f_[0] / 2;
-    f_[1] = f(x_[0], t + h / 2);
-    x_[1] = x + h * f_[1] / 2;
-    f_[2] = f(x_[1], t + h / 2);
-    x_[2] = x + h * f_[2];
-    f_[3] = f(x_[2], t + h);
-    return x + h * (f_[0] + 2 * f_[1] + 2 * f_[2] + f_[3]) / 6;
+    var xs_temp = Euler(xs, h, t);
+    var xs_next = new double[xs.Length];
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * (fs[i](xs, t) + fs[i](xs_temp, t + h)) / 2.0;
+    return xs_next;
 }
-
-const double t0 = 0, t1 = 1.6;  // 定義域
-const double x0 = 0;            // 初期値
-const double h = 0.05;          // 刻み幅
-const double e = 0.001;         // 定義域拡大量(浮動小数点の誤差による終端部抜け防止)
-var methodName = new String[] { "オイラー法：", "修正オイラー法：", "ルンゲクッタ法："};
-
-for (int i = 0; i < methodName.Length; i++)
+double[] RungeKutta(double[] xs, double h, double t)     // ルンゲクッタ法
 {
-    Console.WriteLine(methodName[i]);
-    double x = x0, error = 0;
+    double[,] fss = new double[xs.Length, 4];
+    double[] xs_next = new double[xs.Length];
+    for (int i = 0; i < xs.Length; ++i) fss[i, 0] = fs[i](xs, t);
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * fss[i, 0] / 2;
+    for (int i = 0; i < xs.Length; ++i) fss[i, 1] = fs[i](xs_next, t + h / 2);
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * fss[i, 1] / 2;
+    for (int i = 0; i < xs.Length; ++i) fss[i, 2] = fs[i](xs_next, t + h / 2);
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * fss[i, 2];
+    for (int i = 0; i < xs.Length; ++i) fss[i, 3] = fs[i](xs_next, t + h);
+    for (int i = 0; i < xs.Length; ++i) xs_next[i] = xs[i] + h * (fss[i, 0] + 2 * fss[i, 1] + 2 * fss[i, 2] + fss[i, 3]) / 6;
+    return xs_next;
+}
+const double t0 = 0, t1 = 30.0, h = 0.01, e = 0.001;
+var fileNames = new String[] { "オイラー法.csv", "修正オイラー法.csv", "ルンゲクッタ法.csv" };
+var methods = new Func<double[], double, double, double[]>[] { Euler, ModifiedEuler, RungeKutta };
+for (int i = 0; i < fileNames.Length; i++)
+{
+    var xs = new double[] { 3, 0, 0.3, 0.2 };   // (x, y, u, v)の初期値
+    var sb = new System.Text.StringBuilder();
+    sb.AppendLine("t,x,y");
     for (double t = t0; t <= t1 + e; t += h)
     {
-        error = Math.Max(Math.Abs(x - Math.Tanh(t)), error);    // 厳密解tanh(t)との差
-        if (i == 0) x = Euler(x, h, t);
-        else if (i == 1) x = ModifiedEuler(x, h, t);
-        else if (i == 2) x = RungeKutta(x, h, t);
+        sb.AppendLine(t + "," + xs[0] + "," + xs[1]);
+        xs = methods[i](xs, h, t);
     }
-    Console.WriteLine("誤差の最大値 = " + error.ToString("F10") + Environment.NewLine);
+    File.WriteAllText(fileNames[i], sb.ToString());
 }
