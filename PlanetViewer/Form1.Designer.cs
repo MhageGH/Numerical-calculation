43a5306a3c562062be55d29282612bfa85b750c1
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-10 23:58:38 +0900

    惑星の軌道計算の結果を表示するフォームアプリを追加

---
+++ PlanetViewer/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace PlanetViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(439, 324);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(457, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(439, 324);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox3.Location = new System.Drawing.Point(902, 12);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(439, 324);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 339);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 31);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "オイラー法";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(457, 339);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 31);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "修正オイラー法";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("メイリオ", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(902, 339);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 31);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "ルンゲクッタ法";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1361, 371);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ PlanetViewer/Form1.cs
@@ -0,0 +1,82 @@
+namespace PlanetViewer
+{
+    public partial class Form1 : Form
+    {
+        List<double>[] t = new List<double>[3], x = new List<double>[3], y = new List<double>[3];
+        int currentNumber = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var filename = new String[] { "オイラー法.csv", "修正オイラー法.csv", "ルンゲクッタ法.csv" };
+            for (int i = 0; i < filename.Length; i++)
+            {
+                t[i] = new List<double>();
+                x[i] = new List<double>();
+                y[i] = new List<double>();
+                var sr = File.OpenText(filename[i]);
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine();
+                    if (line != null)
+                    {
+                        var word = line.Split(',');
+                        if (word[0] != "t")
+                        {
+                            t[i].Add(Convert.ToDouble(word[0]));
+                            x[i].Add(Convert.ToDouble(word[1]));
+                            y[i].Add(Convert.ToDouble(word[2]));
+                        }
+                    }
+                }
+            }
+        }
+
+        void Draw(int methodNo, PaintEventArgs e)
+        {
+            var points = new PointF[currentNumber];
+            for (int i = 0; i < currentNumber; i++)
+            {
+                points[i].X = 100.0f * (float)x[methodNo][i] + 50.0f;
+                points[i].Y = -100.0f * (float)y[methodNo][i] + 150.0f;
+            }
+            if (currentNumber != 0)
+            {
+                e.Graphics.DrawLines(Pens.White, points);
+                float r = 10.0f;
+                e.Graphics.FillEllipse(Brushes.DeepSkyBlue, points[currentNumber - 1].X - r, points[currentNumber - 1].Y - r, 2 * r, 2 * r);
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(0, e);
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(1, e);
+        }
+
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+            Draw(2, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var pictureBoxs = new PictureBox[] { pictureBox1, pictureBox2, pictureBox3 };
+            foreach (var pictureBox in pictureBoxs) pictureBox.Invalidate();
+            currentNumber += 10;
+            if (currentNumber >= t[0].Count)
+            {
+                timer1.Stop();
+                currentNumber = currentNumber = t[0].Count - 1;
+            }
+        }
+    }
+}
