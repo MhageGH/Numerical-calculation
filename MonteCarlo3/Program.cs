4e0a472e1aa1aa95d15accb8922ec54305d81124
Mhage <masahage@soleil.ocn.ne.jp>
2022-10-14 23:04:08 +0900

    数値積分

---
+++ MonteCarlo3/Program.cs
@@ -0,0 +1,17 @@
+﻿double f(double[] xs)   // f(x1,x2, …, x10) = x1x2…x10
+{
+    double p = 1;
+    for (int i = 0; i < xs.Length; ++i) p *= xs[i];
+    return p;
+}
+
+var random = new Random();
+int N = 1000000;
+double s = 0;
+for (int i = 0; i < N; ++i)
+{
+    var xs = new double[10];
+    for (int j = 0; j < xs.Length; ++j) xs[j] = random.NextDouble();
+    s += f(xs) * (1.0 / N);
+}
+Console.WriteLine(s);
